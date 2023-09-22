83df25f9ffeb4f35ee3bfc13e9efdc2692a4d022
pawas <pawas.saxena@gmail.com>
2023-09-22 15:09:36 +0530

    added consumer application

--- appConsumer/consumer.js
+++ appConsumer/consumer.js
@@ -1,14 +1,38 @@
 const { Kafka } = require("kafkajs");
 
-const consumer = kafka.consumer({ groupId: 'demo-customer-consumer-group' })
+// Initialize a Kafka client
+const kafka = new Kafka({
+  clientId: "my-demo-customer-producer",
+  brokers: ["kafka:9092"],
+});
 
-await consumer.connect()
-await consumer.subscribe({ topic: 'demo-customer-topic', fromBeginning: true })
+const consumer = kafka.consumer({ groupId: "demo-customer-consumer-group" });
 
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log({
-      value: message.value.toString(),
-    })
-  },
-})
+const consume = async () => {
+  await consumer.connect();
+  console.log("consumer connected successfully");
+  await consumer.subscribe({
+    topic: "demo-customer-topic",
+    fromBeginning: true,
+  });
+  console.log("consumer subscribed successfully");
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+        partition,
+      });
+    },
+  });
+
+  console.log("consumer started listening successfully");
+};
+
+(async () => {
+  try {
+    await consume();
+  } catch (err) {
+    console.log("Consumer error", err);
+  }
+})();
